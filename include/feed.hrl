5332948ef6afffa39273aab09b8702c3c11e7141
Andrii Zadorozhnii <doxtop@synrc.com>
2013-11-03 19:18:35 +0200

    Update feed.hrl

--- include/feed.hrl
+++ include/feed.hrl
@@ -2,7 +2,7 @@
 -define(TEMP_ID, wf:temp_id()).
 -endif.
 
--record(feed_ui,        {?ELEMENT_BASE(feed_ui), state, icon="", icon_url, header=[], selection_ctl=[]}).
+-record(feed_ui,        {?ELEMENT_BASE(feed_ui), state, icon="", icon_url, header=[], selection_ctl=[],title=""}).
 -record(feed_entry,     {?ELEMENT_BASE(feed_ui), entry, state}).
 -record(row_entry,      {?ELEMENT_BASE(feed_ui), entry, state}).
 -record(div_entry,      {?ELEMENT_BASE(feed_ui), entry, state}).
